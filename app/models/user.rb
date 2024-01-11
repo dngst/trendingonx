e9fe1d0b06de5390a5d0a4ba100a456d1a334eb7
dngst <dngst@protonmail.com>
2024-01-11 16:18:07 +0300

    Add devise

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  before_validation :generate_username, on: :create
+
+  private
+
+  def generate_username
+    self.username = "user_#{SecureRandom.hex(8)}"
+  end
+end
