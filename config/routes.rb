4185ebc03378fa0d1c7f17a8290a7c6e93f6e735
dngst <dngst@protonmail.com>
2024-01-14 20:40:32 +0300

    Users can edit and view their profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :topics
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
+  resources :users, only: [:show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
