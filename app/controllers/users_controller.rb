cbf5f76859a547337d5cc4cdf621d647d014125e
dngst <dngst@protonmail.com>
2024-01-15 23:03:05 +0300

    UI changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
+    @topics = Topic.where(user_id: @user.id).order(created_at: :desc)
   end
 end
