7360c9e9a302317b54439cbfff51de85915560cc
dngst <dngst@protonmail.com>
2024-01-25 23:24:23 +0300

    Add strategy

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -1,15 +1,78 @@
+// Latest version of Workbox
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js",
+);
+
+// Workbox modules
+const { CacheFirst, NetworkFirst } = workbox.strategies;
+const { registerRoute, setCatchHandler } = workbox.routing;
+const { warmStrategyCache } = workbox.recipes;
+
+// Event listeners
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+self.addEventListener("fetch", onFetch);
+
+// Cache
+const documentCache = "documents";
+const assetsCache = "assets-styles-and-scripts";
+const imagesCache = "assets-images";
+
+const urls = ["/offline.html"];
+
+// Event handlers
 function onInstall(event) {
-  console.log('[Serviceworker]', "Installing!", event);
+  console.log("[Service Worker]", "Installing!", event);
 }
 
 function onActivate(event) {
-  console.log('[Serviceworker]', "Activating!", event);
+  console.log("[Service Worker]", "Activating!", event);
 }
 
 function onFetch(event) {
-  console.log('[Serviceworker]', "Fetching!", event);
+  console.log("[Service Worker]", "Fetching!", event);
 }
 
-self.addEventListener('install', onInstall);
-self.addEventListener('activate', onActivate);
-self.addEventListener('fetch', onFetch);
+// Cache strategies and routes
+registerRoute(
+  ({ request, url }) =>
+    request.destination === "document" || request.destination === "",
+  new NetworkFirst({
+    cacheName: documentCache,
+  }),
+);
+
+registerRoute(
+  ({ request }) =>
+    request.destination === "script" || request.destination === "style",
+  new CacheFirst({
+    cacheName: assetsCache,
+  }),
+);
+
+registerRoute(
+  ({ request }) => request.destination === "image",
+  new CacheFirst({
+    cacheName: imagesCache,
+  }),
+);
+
+// Offline fallback
+const offlineFallbacStrategy = new CacheFirst();
+
+warmStrategyCache({ urls, offlineFallbackStrategy });
+
+setCatchHandler(async ({ event }) => {
+  switch (event.request.destination) {
+    case "document":
+      return offlineFallbackStrategy.handle({
+        event,
+        request: offlineFallbackUrls[0],
+      });
+    default:
+      return new Response("Not Found", {
+        status: 404,
+        statusText: "Not Found",
+      });
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "topics#index"
   get 'search', to: 'search#index'
-
-  get "/service-worker.js" => "service_worker#service_worker"
-  get "/manifest.json" => "service_worker#manifest"
+  get '/service-worker.js' => 'service_worker#service_worker'
+  get '/manifest.json' => 'service_worker#manifest'
 end
