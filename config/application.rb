dabb121af041606bf823ff77f4e7102b0498190a
dngst <dngst@protonmail.com>
2024-01-27 12:51:59 +0300

    Add custom error pages

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,9 @@ module Trendingonx
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.exceptions_app = ->(env) {
+      ErrorsController.action(:show).call(env)
+    }
   end
 end
