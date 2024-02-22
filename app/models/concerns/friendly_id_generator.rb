022e41bd145ae41438686325b4255e16ccf68de4
dngst <dngst@protonmail.com>
2024-02-22 20:58:08 +0300

    Move friendly_id_generator into a concern

--- app/models/concerns/friendly_id_generator.rb
+++ app/models/concerns/friendly_id_generator.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module FriendlyIdGenerator
+  extend ActiveSupport::Concern
+
   class ReusableSlugGenerator < FriendlyId::SlugGenerator
     def available?(slug)
       if @config.uses?(::FriendlyId::Reserved) && @config.reserved_words.present? && @config.treat_reserved_as_conflict && @config.reserved_words.include?(slug)
